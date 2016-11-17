157fbd6caab4e541508bfec0094c80fbe8a7a941
Sigmund Chow <Sigmund.Chow@hsc.utah.edu>
2016-11-17 14:32:20 -0700

    Fixed rent chart to use rent data. Only displayig California's ZRI now.

--- src/public/js/rentChart.js
+++ src/public/js/rentChart.js
@@ -30,11 +30,11 @@ RentChart.prototype.init = function () {
     self.svg.append("g").attr("id", "yAxis");
     self.svg.append("g").attr("id", "line");
 
-    d3.csv("data/State_Zhvi_AllHomes.csv", function(error, data) {
+    d3.csv("data/State_Zri_AllHomes.csv", function(error, data) {
         var selectedState = data[0];
         console.log(selectedState);
 
-        self.yearsDomain = [];
+        self.datesDomain = [];
         self.formatData = {
             RegionName: selectedState['RegionName'],
             abbr: selectedState['abbr'],
@@ -42,16 +42,20 @@ RentChart.prototype.init = function () {
         };
         self.maxValue = 0;
 
-        for (var i = 1996; i < 2017; i++) {
-            self.yearsDomain.push(i);
-            self.formatData['series'].push({ year: i, price: selectedState[i]});
-            self.maxValue = selectedState[i] > self.maxValue ? selectedState[i] : self.maxValue;
+        var skip = ['RegionName', 'abbr', 'SizeRank'];
+
+        for (var key in selectedState) {
+            if (skip.indexOf(key) == -1) { // Skip non date entries
+                self.datesDomain.push(key);
+                self.formatData["series"].push({ date: key, price: selectedState[key] });
+                self.maxValue = selectedState[key] > self.maxValue ? selectedState[key] : self.maxValue;
+            }
         }
 
         console.log(self.formatData);
 
         self.xScale = d3.scaleBand()
-            .domain(self.yearsDomain)
+            .domain(self.datesDomain)
             .range([0, self.svgWidth - self.xAxisWidth]);
         self.yScale = d3.scaleLinear()
             .domain([0, self.maxValue])
@@ -60,12 +64,10 @@ RentChart.prototype.init = function () {
         // define the line
         self.valueLine = d3.line()
             .x(function (d) {
-                return self.xScale(d.year) + 12;
-                // return d.year;
+                return self.xScale(d.date) + 12;
             })
             .y(function (d) {
                 return self.yScale(d.price);
-                // return d.price;
             });
 
         // Draw line
@@ -98,7 +100,7 @@ RentChart.prototype.init = function () {
             .attr("transform", "translate(" + self.margin.left + "," + self.margin.top + ")")
             .attr("r", 3)
             .attr("cx", function (d) {
-                return self.xScale(d.year) + 12;
+                return self.xScale(d.date) + 12;
             })
             .attr("cy", function (d) {
                 return self.yScale(d.price);
