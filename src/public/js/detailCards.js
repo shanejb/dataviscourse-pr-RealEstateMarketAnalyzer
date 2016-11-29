24715c4d9cfaabcfedb190581ed35d2719315b2d
Shane Brown <shane.brown@utah.edu>
2016-11-29 11:41:13 -0700

    Added selected state cards to visualization

--- src/public/js/detailCards.js
+++ src/public/js/detailCards.js
@@ -20,4 +20,39 @@ DetailCards.prototype.init = function () {
  */
 DetailCards.prototype.update = function (selectedStates) {
     var self = this;
+
+    var cards = d3.select("#detail-cards");
+
+    d3.csv("data/states_facts.csv", function(error, data){
+
+        //  Get the state data
+        for (var abbr in selectedStates) {
+            // Get state data
+            var state = data.filter(function (d) {
+                return d.abbr == selectedStates[abbr]
+            });
+        };
+
+        state = state[0];
+
+        var div = cards.append("div")
+            .attr("id",state["abbr"])
+            .attr("class","card card-inverse")
+            .attr("style","background-color: #333; border-color: #333;");
+
+        div.append("h4")
+            .attr("class","card-title")
+            .text(state.name);
+
+        var ul = div.append("ul");
+        ul.append("li").attr("class","card-text").text("Capital: " + state.capital_city);
+        ul.append("li").attr("class","card-text").text("Largest City: " + state.largest_city);
+        ul.append("li").attr("class","card-text").text("Statehood: " + state.statehood);
+        ul.append("li").attr("class","card-text").text("Population (2015): " + state.population2015);
+        ul.append("li").attr("class","card-text").text("Area: " + state.area_sqmi + " Square Miles");
+        ul.append("li").attr("class","card-text").text("Land Area: " + state.land_area_sqmi + " Square Miles");
+        ul.append("li").attr("class","card-text").text("Water Area: " + state.water_area_sqmi + " Square Miles");
+        ul.append("li").attr("class","card-text").text("Representives: " + state.reps);
+    });
+
 };
