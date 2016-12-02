133841afd34354343841e526c5abd1df1273a8ad
My Huynh <my.huynh@utah.edu>
2016-12-01 18:30:58 -0700

    add tooltips, axes labels, fixed color
    Bubble chart

--- src/public/js/bubbleChart.js
+++ src/public/js/bubbleChart.js
@@ -4,7 +4,7 @@
 function BubbleChart() {
     var self = this;
     self.init();
-};
+}
 
 /**
  * Initializes the svg elements required to for this chart
@@ -15,8 +15,8 @@ BubbleChart.prototype.init = function () {
 
     // Gets access to the div element created for this chart from HTML
     self.svgBounds = divBubbleChart.node().getBoundingClientRect();
-    self.xAxisWidth = 100;
-    self.yAxisHeight = 50;
+    self.xAxisWidth = 40;
+    self.yAxisHeight = 65;
     self.svgWidth = self.svgBounds.width - self.xAxisWidth;
     self.svgHeight = 400;
 
@@ -31,7 +31,6 @@ BubbleChart.prototype.init = function () {
 
     d3.csv("data/State_Zhvi_AllHomes.csv", function(error, data) {
             // Create the x, y and r scales
-            // Create colorScale
 
             self.xScale = d3.scaleBand()
                 .domain(data.map(function (d) {
@@ -45,22 +44,41 @@ BubbleChart.prototype.init = function () {
                 .domain([0, self.maxValue]).range([self.svgHeight - self.yAxisHeight,10]);
             self.rScale = d3.scaleLinear()
                 .domain([1,50])
-                .range([10,1]);
-            self.colorScale = d3.scaleLinear()
-                .domain([1, 10])
-                .range(["#800026","#bd0026","#e31a1c","fc4e2a","fd8d3c"]);
+                .range([15,1]);
+           // self.colorScale = d3.scaleLinear()
+             //   .domain([1, 10])
+             //   .range(["#800026","#bd0026","#e31a1c","fc4e2a","fd8d3c"]);
 
             // Create the axes
             self.xAxis = d3.axisLeft()
                 .scale(self.xScale);
             d3.select("#xAxis_bubble")
                 .attr("transform", "rotate(-90) translate(" + (self.yAxisHeight - self.svgHeight -1) + ",0)")
-                .call(self.xAxis);
+                .call(self.xAxis)
+                .selectAll('text')
+                .attr("x", -5)
+                .attr("y", 11)
+                .attr("transform", "rotate(45)");
             self.yAxis = d3.axisLeft()
                 .scale(self.yScale);
             d3.select("#yAxis_bubble")
                 .attr("transform", "translate(" + self.yAxisHeight + ",0)")
                 .call(self.yAxis);
+
+            // display x,y axis labels
+            self.svg.append("text")
+            //     .attr("class", "")
+                .attr("text-anchor", "end")
+                .attr("x", self.svgWidth)
+                .attr("y", self.svgHeight - 25)
+                .text("State Size Rank");
+            self.svg.append("text")
+            //  .attr("class", "")
+                .attr("text-anchor", "end")
+                .attr("y", 0)
+                .attr("dy", ".75em")
+                .attr("transform", "rotate(-90)")
+                .text("ZHVI house cost ($)");
 
             // Draw bubble chart for the first time
             //Start off with year 1996 selected
@@ -68,11 +86,12 @@ BubbleChart.prototype.init = function () {
                 .data(data)
                 .enter().append("circle")
                 .attr("class", "circle")
-                .attr("cx", function(d) { return self.xScale(d.SizeRank) + 7})
+                .attr("cx", function(d) { return self.xScale(d.SizeRank) + 14})
                 .attr("r", function(d) { return self.rScale(d.SizeRank)})
                 .attr("cy", function(d) { return self.yScale(d["1996"]); })
-              .style("fill", function(d) { return self.colorScale(d.SizeRank)});
-
+            //  .style("fill", function(d) { return self.colorScale(d.SizeRank)});
+                .attr("name", function(d) { return d.RegionName; })
+                .style("fill", "#00688B");
            d3.select("#slider").on("input", function() {
                self.update(+this.value);
             }
@@ -90,15 +109,43 @@ BubbleChart.prototype.update = function (selectedYear) {
     var self = this;
     d3.select("#year").text(selectedYear);
     var cy = d3.select("#slider").property("value", selectedYear);
+    var div = d3.select(".tooltip");
     var year= 1996;
   //  var year2 = year + 1;
-   d3.select("#circles").selectAll(".circle")
-        .attr("cy", function(d) {
-            for (i = 0; i <= 20; i++)  {
-                var year2 = year + i;
-                if (selectedYear == year2) {
-                    return self.yScale(d[year2]);
-                }
-            }
-        })
-    };
+   d3.select("#circles").selectAll("circle")
+       .attr("cy", function(d) {
+           for (i = 0; i <= 20; i++)  {
+               var year2 = year + i;
+               if (selectedYear == year2) {
+                   return self.yScale(d[year2]);
+               }
+           }
+       })
+       .attr("value", function(d) {
+           for (i = 0; i <= 20; i++) {
+             //  var value;
+               var year2 = year + i;
+               if (selectedYear == year2) {
+                   if (d[year2] < 1)
+                       return "No Data";
+                   else
+                       //return "$" + d[year2];
+                       return "$"+ d[year2].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+               }
+           }
+       }
+       )
+       .on("mouseover", function() {
+           div.transition()
+               .duration(200)
+               .style("opacity", .9);
+           div.html(d3.select(d3.event.target).attr("name") + "<br/>" + d3.select(d3.event.target).attr("value"))
+               .style("left", (d3.event.pageX) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+       })
+       .on("mouseout", function() {
+           div.transition()
+               .duration(500)
+               .style("opacity", 0);
+       });
+  };
